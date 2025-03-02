bbcab4aee0eb96c8e2a75853d533d25ffbc2ddc9
Mehdirps <mehdi.raposo77@gmail.com>
2025-03-02 18:58:12 +0100

    🎨

--- routes/api.php
+++ routes/api.php
@@ -127,39 +127,6 @@ Route::middleware('api')->group(function () {
         // Route::post('datas/export', [App\Http\Controllers\DashboardController::class, 'exportDatas']);
     });
 
-    /* Admin */
-    Route::prefix('admin')->middleware('auth.jwt')->group(function () {
-        Route::get('/', [App\Http\Controllers\AdminController::class, 'index'])->name('admin.index');
-
-        /* Users */
-        Route::get('users', [App\Http\Controllers\AdminController::class, 'users'])->name('admin.users.index');
-        Route::get('users/enable/{id}', [App\Http\Controllers\AdminController::class, 'enableUser'])->name('admin.user.enable');
-        Route::get('/admin/user/search', [App\Http\Controllers\AdminController::class, 'searchUsers'])->name('admin.user.searchUsers');
-
-        /* Backgrounds */
-        Route::get('backgrounds', [App\Http\Controllers\AdminController::class, 'backgrounds'])->name('admin.backgrounds.index');
-        Route::post('background_groups', [App\Http\Controllers\AdminController::class, 'addBackgroundGroup'])->name('admin.background_groups.add');
-        Route::get('background_groups/{id}', [App\Http\Controllers\AdminController::class, 'deleteBackgroundGroup'])->name('admin.background_groups.delete');
-        Route::post('backgrounds', [App\Http\Controllers\AdminController::class, 'addBackground'])->name('admin.backgrounds.add');
-        Route::get('backgrounds/{id}', [App\Http\Controllers\AdminController::class, 'deleteBackground'])->name('admin.backgrounds.delete');
-
-        /* Livrets */
-        Route::get('livrets', [App\Http\Controllers\AdminController::class, 'livrets'])->name('admin.livrets.index');
-        Route::get('livret/search', [App\Http\Controllers\AdminController::class, 'searchLivrets'])->name('admin.livret.searchLivrets');
-
-        /* Products */
-        Route::get('products', [App\Http\Controllers\AdminController::class, 'products'])->name('admin.products.index');
-        Route::post('products', [App\Http\Controllers\AdminController::class, 'addProduct'])->name('admin.products.add');
-        Route::get('products/{id}', [App\Http\Controllers\AdminController::class, 'deleteProduct'])->name('admin.products.delete');
-        Route::post('products/update', [App\Http\Controllers\AdminController::class, 'updateProduct'])->name('admin.products.update');
-        Route::post('products/search', [App\Http\Controllers\AdminController::class, 'searchProducts'])->name('admin.products.searchProducts');
-
-        /* Product Catégories */
-        Route::post('product_categories', [App\Http\Controllers\AdminController::class, 'addProductCategory'])->name('admin.product_categories.add');
-        Route::get('product_categories/{id}', [App\Http\Controllers\AdminController::class, 'deleteProductCategory'])->name('admin.product_categories.delete');
-        Route::post('product_categories/update', [App\Http\Controllers\AdminController::class, 'updateProductCategory'])->name('admin.product_categories.update');
-    });
-
     Route::post('/stripe-intent', [App\Http\Services\StripeService::class, 'stripeIntent']);
 
     Route::post('/send-confirmation-email', [App\Http\Controllers\OrderController::class, 'sendConfirmationEmail']);
