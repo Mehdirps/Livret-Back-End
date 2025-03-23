944ec77d63ce09678d2101c72bfe789ceb475525
Mehdirps <mehdi.raposo77@gmail.com>
2025-03-23 18:55:55 +0100

    🎨 copyright

--- config/logging.php
+++ config/logging.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Copyright (c) 2025 Mehdi Raposo
+ * Ce fichier fait partie du projet Heberginfos.
+ *
+ * Ce fichier, ainsi que tout le code et les ressources qu'il contient,
+ * est protégé par le droit d'auteur. Toute utilisation, modification,
+ * distribution ou reproduction non autorisée est strictement interdite
+ * sans une autorisation écrite préalable de Mehdi Raposo.
+ */
 
 use Monolog\Handler\NullHandler;
 use Monolog\Handler\StreamHandler;
